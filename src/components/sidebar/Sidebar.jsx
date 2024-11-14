2b9610576def2bb04f3ebb846e523b23ee644d89
DilanFdo <priyankarad276@gmail.com>
2024-11-14 23:29:30 +0530

    icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import './Sidebar.scss';
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LoginIcon from '@mui/icons-material/Login';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
 const Sidebar = () => {
   return (
@@ -15,36 +26,47 @@ const Sidebar = () => {
                     <span>Dashboard</span>
                 </li>
                 <li>
+                    <PersonIcon/>
                     <span>Users</span>
                 </li>
                 <li>
+                    <Inventory2Icon/>
                     <span>Products</span>
                 </li>
                 <li>
+                    <CreditCardIcon/>
                     <span>Orders</span>
                 </li>
                 <li>
+                    <LocalShippingIcon/>
                     <span>Delivery</span>
                 </li>
                 <li>
+                    <InsertChartIcon/>
                     <span>Stats</span>
                 </li>
                 <li>
+                    <NotificationsIcon/>
                     <span>Notifications</span>
                 </li>
                 <li>
+                    <SettingsSystemDaydreamIcon/>
                     <span>System Health</span>
                 </li>
                 <li>
+                    <LoginIcon/>
                     <span>Logs</span>
                 </li>
                 <li>
+                    <SettingsIcon/>
                     <span>Settings</span>
                 </li>
                 <li>
+                    <AccountCircleIcon/>
                     <span>Profile</span>
                 </li>
                 <li>
+                    <LogoutIcon/>
                     <span>Logout</span>
                 </li>
             </ul>
