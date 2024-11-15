df0ad423688ac6750dd2c227dee207a22f291bce
DilanFdo <priyankarad276@gmail.com>
2024-11-15 23:34:05 +0530

    navbar create

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './Navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ListIcon from '@mui/icons-material/List';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...' />
+          <SearchIcon/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon/>
+            English
+          </div>
+          <div className="item">
+            <FullscreenExitIcon/>
+          </div>
+          <div className="item">
+            <NotificationsIcon/>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineIcon/>
+          </div>
+          <div className="item">
+            <ListIcon/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './home.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 const home = () => {
   return (
     <div className='home'>
         <Sidebar/>
-        <div className="homeContainer">container</div>
+        <div className="homeContainer">
+          <Navbar/>
+          Home container
+        </div>
     </div>
   )
 }
