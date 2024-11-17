5e9be9e8119ff3b562c9b14ee554814286f68027
DilanFdo <priyankarad276@gmail.com>
2024-11-17 21:54:29 +0530

    widget style

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './widget.scss';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
 const widget = () => {
   return (
@@ -9,7 +11,13 @@ const widget = () => {
             <span className='counter'>123456</span>
             <span className='link'>See all Users</span>
         </div>
-        <div className="right">right</div>
+        <div className="right">
+          <div className="percentage">
+            <KeyboardArrowUpIcon/>
+            20%</div>
+            <PersonOutlineIcon className='icon'/>
+        </div>
+        
     </div>
   )
 }
