e27e28ab06ddf7f3db4d4eaf9bc77f8b15001093
DilanFdo <priyankarad276@gmail.com>
2024-11-30 19:14:39 +0530

    circularprogressbar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,8 @@ import './home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import Widget from '../../components/widget/widget';
+import Featured from '../../components/featured/featured';
+import Chart from '../../components/chart/chart';
 
 
 const home = () => {
@@ -17,8 +19,9 @@ const home = () => {
             <Widget type="earnings"/>
             <Widget type="balance"/>
           </div>
-          <div className="chats">
-            
+          <div className="charts">
+            <Featured/>
+            <Chart/>
           </div>
         </div>
     </div>
