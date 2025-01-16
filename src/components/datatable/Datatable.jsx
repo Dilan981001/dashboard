94e107d10facac0096244ce6fafc68f4e021adde
DilanFdo <priyankarad276@gmail.com>
2025-01-16 19:42:28 +0530

    add view, deletebtn

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -3,18 +3,25 @@ import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 import { userColumns, userRows }  from '../../datatablesource';
 
-const paginationModel = { page: 0, pageSize: 5 };
+const paginationModel = { page: 0, pageSize: 15 };
 const Datatable = () => {
 
-    
+  const actionColumn = [{field:"action" , headerName: "Action", width:200 , renderCell:()=>{
+    return (
+      <div className="cellAction">
+        <div className="viewButton">View</div>
+        <div className="deleteButton">Delete</div>
+      </div>
+    )
+  }}] 
   return (
     <div className='datatable'>
-      <Paper sx={{ height: 400, width: '100%' }}>
+      <Paper sx={{ height: 800, width: '100%' }}>
       <DataGrid
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[5, 15]}
         checkboxSelection
         sx={{ border: 0 }}
       />
