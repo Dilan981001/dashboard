8d9f621201bcc7fce43b58add1f517468f0265c2
DilanFdo <priyankarad276@gmail.com>
2025-01-17 12:37:44 +0530

    single page add chart

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -22,7 +22,7 @@ const home = () => {
           </div>
           <div className="charts">
             <Featured/>
-            <Chart/>
+            <Chart aspect={2/1}/>
           </div>
           <div className="listContainer">
             <div className="listTitle">Latest Transaction</div>
